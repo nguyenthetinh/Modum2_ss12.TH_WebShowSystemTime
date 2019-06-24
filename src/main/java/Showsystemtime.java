16116460e5f7766a1fc6faa18a5a46e0244c4521
nguyenthetinh <tinhnt0207@gmail.com>
2019-06-24 21:51:50 +0700

    bai

---
+++ src/main/java/Showsystemtime.java
@@ -0,0 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class Showsystemtime extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getOutputStream().println(String.valueOf(LocalDateTime.now()));
+    }
+}
